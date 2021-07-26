a17b20dcf5ff71e2c8b4578e3dc8ec927e07a932
Erico Murilo Gozzi <erico@llee.tech>
2021-07-26 11:08:05 -0300

    Update lgpdTermo.js
    Comportamento padrão do componente é ficar escondido se não for inicializado.

--- src/js/lgpdTermo.js
+++ src/js/lgpdTermo.js
@@ -3,7 +3,7 @@
  *
  * Description. Biblioteca para tratamento do termo de consentimento guardando os dados apenas localmente.
  *
- * @version 1.1
+ * @version 1.1.1
  * @link   https://github.com/egozzi/lgpd-cookie.git
  * @author Erico Murilo Gozzi
  * @license	LGPL-3.0-or-later
@@ -13,85 +13,94 @@
 
 const termoConsentimentoLgpd = (function () {
 
-  let CHAVE_TERMO = "LGPD.TERMO";
+    let CHAVE_TERMO = "LGPD.TERMO";
 
-  const iniciarComponente = function (chaveTermo) {
+    const iniciarComponente = function (chaveTermo) {
 
-    if (chaveTermo)
-        CHAVE_TERMO = chaveTermo;
+        if (chaveTermo)
+            CHAVE_TERMO = chaveTermo;
 
-    var botaoTermo = document.getElementById("lgpd-botao");
+        var botaoTermo = document.getElementById("lgpd-botao");
 
-    if (botaoTermo) {
+        if (botaoTermo) {
 
-        botaoTermo.onclick = lgpdCliqueTermo;
+            botaoTermo.onclick = lgpdCliqueTermo;
 
-      if (aceitouTermo()) {
-        lgpdDesativaTermo();
-      }
-    } else {
-      console.error("[COMPONENTE CONSENTIMENTO LGPD] Botão do termo não encontrado na página. Funcionalidade não funcionará adequadamente");
-    }
-  };
-
-  const lgpdCliqueTermo = function () {
-
-    let dataAceite = dataAtual();
-    setCookie(CHAVE_TERMO, "Aceito em " + dataAceite);
-
-    lgpdDesativaTermo();
-  };
-
-  const monitorarElemento = function (id, callbackOk, callbackNOk) {
-    var elemento = document.getElementById(id);
-
-    if (elemento) {
-      elemento.addEventListener("click", function () {
-        if (!aceitouTermo()) {
-          if (callbackNOk) {
-            callbackNOk();
-          } else {
-            alert(
-              "Você deve aceitar o termo de consentimento para realizar esta operação"
-            );
-          }
-          return false;
+            if (aceitouTermo()) {
+                lgpdDesativaTermo();
+            } else {
+                lgpdAtivaTermo();
+            }
         } else {
-          if (callbackOk) {
-            callbackOk();
-          }
+            console.log("[COMPONENTE CONSENTIMENTO LGPD] Termo não encontrado. Componente desativado.");
+            lgpdDesativaTermo();
         }
-      });
+    };
+
+    const lgpdCliqueTermo = function () {
+
+        let dataAceite = dataAtual();
+        setCookie(CHAVE_TERMO, "Aceito em " + dataAceite);
+
+        lgpdDesativaTermo();
+    };
+
+    const monitorarElemento = function (id, callbackOk, callbackNOk) {
+        var elemento = document.getElementById(id);
+
+        if (elemento) {
+            elemento.addEventListener("click", function (evt) {
+                if (!aceitouTermo()) {
+                    if (callbackNOk) {
+                        callbackNOk();
+                    } else {
+                        alert(
+                            "Você deve aceitar o termo de consentimento para realizar esta operação"
+                        );
+                    }
+                    evt.preventDefault();
+                    return false;
+                } else {
+                    if (callbackOk) {
+                        callbackOk();
+                    }
+                }
+            });
+        }
+    };
+
+    function aceitouTermo() {
+        return getCookie(CHAVE_TERMO) != null;
     }
-  };
 
-  function aceitouTermo() {
-    return getCookie(CHAVE_TERMO) != null;
-  }
+    function lgpdDesativaTermo() {
+        const termo = document.getElementById("lgpd-termo");
+        termo.style.display = "none";
+    }
 
-  function lgpdDesativaTermo() {
-    const termo = document.getElementById("lgpd-termo");
-    termo.style.display = "none";
-  }
+    function lgpdAtivaTermo() {
+        const termo = document.getElementById("lgpd-termo");
+        termo.style.display = "block";
+    }
 
-  function getCookie(k) {
-    return localStorage.getItem(k);
-  }
+    function getCookie(k) {
+        return localStorage.getItem(k);
+    }
 
-  function setCookie(k, v) {
-    localStorage.setItem(k, v);
-  }
+    function setCookie(k, v) {
+        localStorage.setItem(k, v);
+    }
 
-  function dataAtual(){
+    function dataAtual() {
 
-    var d = new Date();
+        var d = new Date();
 
-    return d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear() + " " 
-         + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
-  } 
+        return d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear() + " "
+            + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    }
 
-  return {
-    iniciarTermo: iniciarComponente,
-    monitorarElemento: monitorarElemento,
-  };
+    return {
+        iniciarTermo: iniciarComponente,
+        monitorarElemento: monitorarElemento,
+    };
 })();
